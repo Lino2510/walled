37bcf720c8ffa34f5e0f4af9c158e6709ea6b66e
lulu <luluulmawarnafis@outlook.com>
2024-12-12 07:51:03 +0700

    new_walled

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 import avatarImg from "../assets/avatar.png";
 
-function Avatar() {
+function Avatar({ name, avatar }) {
   const [isAvatarActive, setIsAvatarActive] = useState(false);
 
   return (
     <div className="flex items-center gap-x-4 ml-auto">
       <span className="text-right">
-        <p className="text-black font-bold">Chelsea Immanuela</p>
-        <p className="text-black">Personal Account</p>
+        <p className="text-black font-bold">{name}</p>
+        <p className="text-black">mawarnafis@gmail.com</p>
       </span>
       <div
         className={`rounded-full border-[6px] hover:border-[6px] hover:border-[#178F8D] cursor-pointer transition-all ${
@@ -17,7 +17,7 @@ function Avatar() {
         }`}
         onClick={() => setIsAvatarActive((prev) => !prev)}
       >
-        <img src={avatarImg} alt="avatar" className="rounded-full" />
+        <img width={150} src={avatar} alt="avatar" className="rounded-full" />
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Avatar from "./Avatar";
+import TableComponent from "../components/Table";
 import viewIcon from "../assets/view.png";
+import { useTheme } from "../hooks/useTheme";
+import { FaBeer } from "react-icons/fa";
+import { useEffect } from "react";
+import Plus from "../assets/plus.png";
+import Plane from "../assets/plane.png";
 
 function Hero() {
   const [showBalance, setShowBalance] = useState(true);
-  const [username, setUserName] = useState("");
+  const { theme } = useTheme();
+  const themeColor = theme === "green" ? "#19918F" : "#007BFF";
+  const user = JSON.parse(localStorage.getItem("loginForm"));
+
+  const userName = user ? user.fullName : "User";
+  const userBalance = user ? user.balance : 0;
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    const loginObj = localStorage.getItem("login");
-    if (loginObj !== null) {
-      const { email } = JSON.parse(loginObj);
-      setUserName(email);
+    async function getData() {
+      const url = "http://localhost:3000/users";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        setUsers(json);
+        console.log(json);
+      } catch (error) {
+        console.error(error.message);
+      }
     }
+    getData();
   }, []);
+
   return (
     <section className="w-full px-16 mt-12">
       <div className="flex items-center justify-center">
         <div className="mr-auto">
           <h1 className="text-black text-6xl font-bold">
-            {`Good Morning ${username}!`}
+            {`Good Morning, ${users[0]?.name}`}
           </h1>
           <p className="text-black text-2xl mt-3">
             Check all your incoming and outgoing transactions here
           </p>
         </div>
-        <Avatar />
+        <Avatar name={users[0]?.name} avatar={users[0]?.avatar} />
       </div>
-
       <div className="flex mt-[4.5rem] gap-x-12">
-        <div className="text-white bg-[#19918F] p-12 rounded-2xl w-1/5">
+        <div
+          className="bg-[#19918F] p-12 rounded-2xl w-1/5 text-white"
+          style={{ backgroundColor: themeColor }}
+        >
           <p>Account No.</p>
           <p className="text-white mt-3 font-bold">100899</p>
         </div>
@@ -35,7 +61,7 @@ function Hero() {
           <p>Balance</p>
           <span className="flex items-center mt-3 gap-x-2">
             <p className="font-bold">
-              {showBalance ? "Rp10.000.000,00" : "Rp ********"}
+              {showBalance ? `Rp ${users[0]?.balance}` : "Rp ********"}
             </p>
             <img
               src={viewIcon}
@@ -45,6 +71,24 @@ function Hero() {
             />
           </span>
         </div>
+      </div>
+      <div className="flex items-center justify-end gap-x-2 mt-[-5rem]">
+        <div
+          className="bg-[#19918F] p-2 rounded-lg w-8 h-8 flex items-center justify-center"
+          style={{ backgroundColor: themeColor }}
+        >
+          <img src={Plus} alt="plus" className="w-4 h-4" />
+        </div>
+        <div
+          className="bg-[#19918F] p-2 rounded-lg w-8 h-8 flex items-center justify-cente"
+          style={{ backgroundColor: themeColor }}
+        >
+          <img src={Plane} alt="plane" className="w-4 h-4" />
+        </div>
+      </div>
+      <h3></h3>
+      <div className="mt-20">
+        <TableComponent />
       </div>
     </section>
   );
